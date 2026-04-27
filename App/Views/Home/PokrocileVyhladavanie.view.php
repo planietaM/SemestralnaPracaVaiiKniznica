be745d0916afd5929f756883b7ffd58d51b47793
planietaM <planieta4@stud.uniza.sk>
2026-04-28 01:06:08 +0200

    inicializacia

---
+++ App/Views/Home/PokrocileVyhladavanie.view.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Framework\Support\LinkGenerator $link */
+?>
+
+<h1>Pokročilé vyhľadavanie</h1>
+
+<div class="row mt-3">
+    <div class="col">
+        <a href="<?= $link->url("home.index") ?>">Back to main page</a>
+    </div>
+</div>
